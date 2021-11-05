e341aa054ae04880e68731c6e77e6a5b1e6d081e
Arthur Mendes <thumendess@gmail.com>
2021-11-05 10:38:59 -0300

    Breakpoints

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -10,7 +10,7 @@ const Block: React.FC<BlockProps> = ({ value, highlight = false, ...rest }) => {
   return (
     <li
       {...rest}
-      className={`w-60 h-60 bg-${blocks[value].color}-${
+      className={`w-40 h-40 md:w-60 md:h-60 bg-${blocks[value].color}-${
         highlight ? "300" : "500"
       } rounded-lg font-bold text-white text-2xl flex items-center justify-center`}
     >
